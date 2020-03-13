eca167bd121ba63d00658ba117b59d8d0acca57e
Jared Blumer <jaredgblumer@gmail.com>
2020-03-13 18:40:52 -0400

    Add font styling

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ function App() {
     <div className="App">
       <div className="name">Freemodoro</div>
       <div className="stop">Stop</div>
-      <div className="timer">25:00</div>
+      <div className="timer">
+        <span>25:00</span>
+      </div>
       <div className="dial"></div>
       <div className="start"></div>
       <div className="round">
         <div className="round-title">Round</div>
         <div className="round-data">
-          <span className="footer-data-curent">0</span>
+          <span className="footer-data-current">0</span>
           <span className="footer-data-goal">/4</span>
         </div>
       </div>
       <div className="goal">
         <div className="goal-title">Goal</div>
         <div className="goal-data">
-          <span className="footer-data-curent">0</span>
+          <span className="footer-data-current">0</span>
           <span className="footer-data-goal">/12</span>
         </div>
       </div>
