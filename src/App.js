72156889bcc02412573373920039963e8bf0c650
Jared Blumer <jaredgblumer@gmail.com>
2020-03-13 17:40:50 -0400

    Add css grid columns

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="name">Freemodoro</div>
+      <div className="stop">Stop</div>
+      <div className="timer">25:00</div>
+      <div className="dial"></div>
+      <div className="start"></div>
+      <div className="round">
+        <div className="round-title">Round</div>
+        <div className="round-data">
+          <span className="footer-data-curent">0</span>
+          <span className="footer-data-goal">/4</span>
+        </div>
+      </div>
+      <div className="goal">
+        <div className="goal-title">Goal</div>
+        <div className="goal-data">
+          <span className="footer-data-curent">0</span>
+          <span className="footer-data-goal">/12</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
