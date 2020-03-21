f0c2402dbb9d7ef3e21ede45d3c687773d36913c
Jared Blumer <jaredgblumer@gmail.com>
2020-03-21 09:45:43 -0400

    Add reset timer test

--- src/components/Pomodoro.test.js
+++ src/components/Pomodoro.test.js
@@ -1,15 +1,29 @@
 "use strict";
 
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { cleanup, fireEvent, render } from "@testing-library/react";
 import Pomodoro from "./Pomodoro";
 
 jest.useFakeTimers();
 
-test("Pressing button triggers timer start", () => {
+afterEach(() => {
+  jest.clearAllTimers();
+});
+
+test("Clicking start button triggers timer start", () => {
   const { getByTestId } = render(<Pomodoro />);
   fireEvent.click(getByTestId("timer-button"));
   jest.advanceTimersByTime(1000);
   let display = getByTestId("timerDisplay");
   expect(display.textContent).toBe("24:59");
+});
+
+test("Clicking timer display resets timer after timer start", () => {
+  const { getByTestId } = render(<Pomodoro />);
+  fireEvent.click(getByTestId("timer-button"));
+  jest.advanceTimersByTime(1000);
+  let display = getByTestId("timerDisplay");
+  expect(display.textContent).toBe("24:59");
+  fireEvent.click(getByTestId("timer-display"));
+  expect(display.textContent).toBe("25:00");
 });
