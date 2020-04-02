1e18d34db4b40ddff013bbd3f8eee6f6111b6f78
Jared Blumer <jaredgblumer@gmail.com>
2020-04-01 20:48:04 -0400

    Add play button svg

--- src/components/ProgressButton.js
+++ src/components/ProgressButton.js
@@ -24,6 +24,8 @@ class ProgressButton extends React.Component {
           cx={radius}
           cy={radius}
         />
+        <circle fill="green" cx={radius} cy={radius} r={radius - 14} />
+        <polygon points="50,50 60,70, 70,50" />
       </svg>
     );
   }
