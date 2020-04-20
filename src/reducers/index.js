e8278a3ee01da3e40ccf3fc877f3b9bcdd1c8d0e
Jared Blumer <jaredgblumer@gmail.com>
2020-04-19 21:05:46 -0400

    Update round and goal increments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,13 +3,26 @@ import { INCREMENT_ROUND } from "../actions/types";
 export default function(state, action) {
   switch (action.type) {
     case INCREMENT_ROUND:
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          currentRound: state.data.currentRound + 1
-        }
-      };
+      if (state.data.currentRound === state.data.totalRound) {
+        return {
+          ...state,
+          data: {
+            ...state.data,
+            currentRound: 0,
+            currentGoal: state.data.currentGoal + 1
+          }
+        };
+      } else {
+        return {
+          ...state,
+          data: {
+            ...state.data,
+            currentRound: state.data.currentRound + 1,
+            currentGoal: state.data.currentGoal + 1
+          }
+        };
+      }
+
     default:
       return state;
   }
